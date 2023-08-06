7af109334c15af92b80c63d661815d91873ef925
axel-paillaud <contact@axelweb.fr>
2023-08-06 18:56:32 +0200

    build a graduation sign ok

--- javascript/custom-signs/custom-signs.js
+++ javascript/custom-signs/custom-signs.js
@@ -39,7 +39,8 @@ export function buildBirthdaySign(age) {
  */
 
 export function graduationFor(name, year) {
-  throw new Error('Implement the graduationForm function');
+	return `Congratulations ${name}!
+Class of ${year}`;
 }
 
 /**
