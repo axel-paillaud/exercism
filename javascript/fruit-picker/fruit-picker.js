22f42280c38af5118452a8ec23d7997198307a43
axel-paillaud <contact@axelweb.fr>
2023-08-10 18:06:12 +0200

    task 1, 2, 3 OK

--- javascript/fruit-picker/fruit-picker.js
+++ javascript/fruit-picker/fruit-picker.js
@@ -16,14 +16,18 @@ import { order } from './grocer';
  * @return void
  */
 export function onSuccess() {
-  // implement the onSuccess callback to call notify with a success message
+	notify({
+		message: 'SUCCESS',
+	});
 }
 
 /**
  * @return void
  */
 export function onError() {
-  // implement the onError callback to call notify with an error message
+	notify({
+		message: 'ERROR',
+	});
 }
 
 /**
@@ -33,7 +37,7 @@ export function onError() {
  * @return void
  */
 export function orderFromGrocer(query, onSuccessCallback, onErrorCallback) {
-  // implement the orderFromGrocer function to order the query
+	order(query, onSuccessCallback, onErrorCallback);
 }
 
 /**
