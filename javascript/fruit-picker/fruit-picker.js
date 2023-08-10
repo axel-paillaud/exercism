d19e555dc617d2ecc40695c448cbb5db7a93481f
axel-paillaud <contact@axelweb.fr>
2023-08-10 18:09:45 +0200

    fruit-picker 4 tasks first version OK

--- javascript/fruit-picker/fruit-picker.js
+++ javascript/fruit-picker/fruit-picker.js
@@ -46,5 +46,6 @@ export function orderFromGrocer(query, onSuccessCallback, onErrorCallback) {
  * @return void
  */
 export function postOrder(variety, quantity) {
-  //implement the postOrder function to create a query and order
+	let query = {variety, quantity};
+	order(query, onSuccess, onError);
 }
