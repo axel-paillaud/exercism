39407590ea98439545764d34f59446a4f5c749a3
axel-paillaud <contact@axelweb.fr>
2023-08-11 16:07:23 +0200

    3. Swap the first two cards OK

--- javascript/elyses-destructured-enchantments/enchantments.js
+++ javascript/elyses-destructured-enchantments/enchantments.js
@@ -33,7 +33,8 @@ export function getSecondCard(deck) {
  * @returns {Card[]} new deck with reordered cards
  */
 export function swapTopTwoCards(deck) {
-  throw new Error('Implement the swapTopTwoCards function');
+	const [firstCard, secondCard, ...remainingCards] = deck;
+	return [secondCard, firstCard, ...remainingCards];
 }
 
 /**
