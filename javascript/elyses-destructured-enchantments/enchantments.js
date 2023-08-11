bf9fde0162674588c7169cacd012103f8266a485
axel-paillaud <contact@axelweb.fr>
2023-08-11 16:11:47 +0200

    5. Insert face cards OK, all exercises OK

--- javascript/elyses-destructured-enchantments/enchantments.js
+++ javascript/elyses-destructured-enchantments/enchantments.js
@@ -62,5 +62,6 @@ const FACE_CARDS = ['jack', 'queen', 'king'];
  * third, and fourth cards are the face cards
  */
 export function insertFaceCards(deck) {
-  throw new Error('Implement the insertFaceCards function');
+	const [firstCard, ...remainingCards] = deck;
+	return [firstCard, ...FACE_CARDS, ...remainingCards];
 }
