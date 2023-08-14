29d3e5775bb732ef0caacb3f6ffb34f0e15383eb
axel-paillaud <contact@axelweb.fr>
2023-08-14 17:55:28 +0200

    move method OK

--- javascript/windowing-system/windowing-system.js
+++ javascript/windowing-system/windowing-system.js
@@ -52,5 +52,21 @@ export class ProgramWindow {
 		else
 			this.size.height = Size.height;
 	}
+
+	move(Position) {
+		if (Position.x < 0)
+			this.position.x = 0;
+		else if (Position.x + this.size.width > this.screenSize.width)
+			this.position.x = this.screenSize.width - this.size.width;
+		else
+			this.position.x = Position.x;
+
+		if (Position.y < 0)
+			this.position.y = 0;
+		else if (Position.y + this.size.height > this.screenSize.height)
+			this.position.y = this.screenSize.height - this.size.height;
+		else
+			this.position.y = Position.y;
+	}
 }
 
