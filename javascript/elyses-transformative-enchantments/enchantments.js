f2532791a48414581388343d3b5d4fb6db669949
axel-paillaud <contact@axelweb.fr>
2023-08-23 13:04:15 +0200

    6. sort number wip

--- javascript/elyses-transformative-enchantments/enchantments.js
+++ javascript/elyses-transformative-enchantments/enchantments.js
@@ -81,7 +81,7 @@ export function twoIsSpecial(deck) {
  * @returns {number[]} ordered deck
  */
 export function perfectlyOrdered(deck) {
-  throw new Error('Implement the perfectlyOrdered function');
+    return deck.sort();
 }
 
 /**
