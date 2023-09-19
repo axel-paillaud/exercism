ee2b2d458df7124c7e8e10cba68bc5e6db80d9f5
axel-paillaud <contact@axelweb.fr>
2023-09-19 06:57:31 +0200

    gigasecond : first exercise OK, need huge refacto

--- javascript/gigasecond/gigasecond.js
+++ javascript/gigasecond/gigasecond.js
@@ -3,6 +3,15 @@
 // convenience to get you started writing code faster.
 //
 
-export const gigasecond = () => {
-  throw new Error('Remove this statement and implement this function');
+export const gigasecond = (date) => {
+    console.log(date);
+    let initDate = new Date(date);
+    let timestamp = Math.floor(initDate.getTime() / 1000);
+    timestamp += 1000000000;
+    timestamp *= 1000;
+    console.log("timestamp : " + timestamp);
+    let newDate = new Date(Math.floor(timestamp));
+    console.log("new date : " + newDate);
+
+    return new Date(timestamp);
 };
