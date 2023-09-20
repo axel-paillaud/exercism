0b235c7b7d64b4ed1b5ec415097b484739a6638b
axel-paillaud <contact@axelweb.fr>
2023-09-20 06:20:19 +0200

    gigasecond in javascript ok

--- javascript/gigasecond/gigasecond.js
+++ javascript/gigasecond/gigasecond.js
@@ -3,15 +3,10 @@
 // convenience to get you started writing code faster.
 //
 
-export const gigasecond = (date) => {
-    console.log(date);
-    let initDate = new Date(date);
-    let timestamp = Math.floor(initDate.getTime() / 1000);
-    timestamp += 1000000000;
-    timestamp *= 1000;
-    console.log("timestamp : " + timestamp);
-    let newDate = new Date(Math.floor(timestamp));
-    console.log("new date : " + newDate);
+const GIGASECOND = 1000000000;
 
+export const gigasecond = (date) => {
+    let timestampInSecond = Math.floor(date.getTime() / 1000);
+    let timestamp = (timestampInSecond + GIGASECOND) * 1000;
     return new Date(timestamp);
 };
