c426a955dc94667f634e68621c6a8dea77093081
axel-paillaud <contact@axelweb.fr>
2023-09-21 07:04:53 +0200

    spage-age exercise ok

--- javascript/space-age/space-age.js
+++ javascript/space-age/space-age.js
@@ -3,6 +3,20 @@
 // convenience to get you started writing code faster.
 //
 
-export const age = () => {
-  throw new Error('Remove this statement and implement this function');
+const EARTH_YEAR_IN_SECOND = 31557600;
+
+const ORBITAL_PERIOD = {
+    earth: 1.0,
+    mercury: 0.2408467,
+    venus: 0.61519726,
+    mars: 1.8808158,
+    jupiter: 11.862615,
+    saturn: 29.447498,
+    uranus: 84.016846,
+    neptune: 164.79132
+}
+
+export const age = (planet, ageInSecond) => {
+    return +(ageInSecond / (EARTH_YEAR_IN_SECOND * ORBITAL_PERIOD[planet]))
+        .toFixed(2);
 };
