64023b30de3eb943a3301e3436b682b989df12d2
axel-paillaud <contact@axelweb.fr>
2023-09-22 06:48:13 +0200

    add another solution with regex and set

--- javascript/pangram/pangram.js
+++ javascript/pangram/pangram.js
@@ -5,12 +5,20 @@
 // I thinks there is 2 ways to solve this : one with classical array checking,
 // and maybe one with regular expression. Let's try both ?
 
+const REGEX = /[a-z]/gmi;
+const ALPHABET_LENGTH = 26;
+
+export const isPangram = (string) => {
+    const UniqueLetters = new Set(string.toLowerCase().match(REGEX));
+    return UniqueLetters.size === ALPHABET_LENGTH;
+}
+
 const ALPHABET = [
     'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 
     'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'
 ];
 
-export const isPangram = (string) => {
+export const isPangram2 = (string) => {
     if (!string) return false;
     for (let i = 0; i < 24; i++) {
         if (!string.toLowerCase().includes(ALPHABET[i])) return false;
