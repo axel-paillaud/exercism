e77229f6e6573dc1cd65b2b396644825d2abb4e7
axel-paillaud <contact@axelweb.fr>
2023-10-03 06:47:31 +0200

    sort book with Set()

--- javascript/book-store/book-store.js
+++ javascript/book-store/book-store.js
@@ -3,14 +3,25 @@
 // convenience to get you started writing code faster.
 //
 
-function sortBook(books) {
-    const set = new Set(books);
-    console.log(books);
-    console.log(set);
-    return set;
+function sortBook(books, sortedBooks = [], index) {
+    let set = new Set(books);
+    set.forEach((sortedBook) =>  {
+        let index = books.findIndex((book) => book === sortedBook);
+        books.splice(index, 1);
+    });
+    sortedBooks[index] = [];
+    sortedBooks[index].push(set);
+    if (books.length === 0) return;
+    else {
+        index++;
+        return sortBook(books, sortedBooks, index);
+    }
 }
 
 export const cost = (books) => {
-    const sortedBook = sortBook(books);
+    let sortedBooks = [];
+    let index = 0;
+    sortBook(books, sortedBooks, index);
+    console.log(sortedBooks);
     return 800;
 };
