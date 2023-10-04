c1177982a6a6739972b6a05c893c9e106db8e2c0
axel-paillaud <contact@axelweb.fr>
2023-10-04 07:07:44 +0200

    first shot of book-store.js ok

--- javascript/book-store/book-store.js
+++ javascript/book-store/book-store.js
@@ -3,6 +3,8 @@
 // convenience to get you started writing code faster.
 //
 
+const BOOK_PRICE = 800;
+
 function sortBook(books, sortedBooks, index) {
     let set = new Set(books);
 
@@ -10,15 +12,28 @@ function sortBook(books, sortedBooks, index) {
         books.splice(books.findIndex((book) => book === sortedBook), 1);
     });
 
-    sortedBooks[index] = set;
+    sortedBooks[index] = [...set];
     
     if (books.length === 0) return sortedBooks;
 
     else return sortBook(books, sortedBooks, ++index);
 }
 
+function sortForBestDiscount(sortedBooks) {
+    sortedBooks.forEach((batch) => {
+        if (batch.length === 5) {
+            sortedBooks.forEach((compareBatch) => {
+                if (compareBatch.length === 3) {
+                    compareBatch.push(batch.pop());
+                }
+            });
+        }
+    });
+    return sortedBooks;
+}
+
 function computeDiscount(batch) {
-    switch(batch.size) {
+    switch(batch.length) {
         case 1:
             return 0;
         case 2:
@@ -32,19 +47,19 @@ function computeDiscount(batch) {
     }
 }
 
-function computePrice(sortedBooks) {
+function computePrice(sortedBooks, bookPrice) {
     let price = 0;
 
     sortedBooks.forEach((batch) => {
         let discount = computeDiscount(batch);
-        price += (batch.size * 800) - ((batch.size * 800) * discount);
+        price += (batch.length * bookPrice) - ((batch.length * bookPrice) * discount);
     });
 
     return price;
 }
 
 export const cost = (books) => {
-    let sortedBooks = sortBook(books, [], 0);
-    let price = computePrice(sortedBooks);
-    return price;
-};
+    if (books.length === 0) return 0;
+    let sortedBooks = sortForBestDiscount(sortBook(books, [], 0));
+    return computePrice(sortedBooks, BOOK_PRICE); 
+}
