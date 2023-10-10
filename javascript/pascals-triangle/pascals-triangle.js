88cf4ec26fa0148e5f827daed04ef58bde6ceb4f
axel-paillaud <contact@axelweb.fr>
2023-10-10 06:59:54 +0200

    refacto pascals-triangle, myself (without mentoring)

--- javascript/pascals-triangle/pascals-triangle.js
+++ javascript/pascals-triangle/pascals-triangle.js
@@ -3,29 +3,21 @@
 // convenience to get you started writing code faster.
 //
 
-function initTriangle() {
-
-}
-
 function computeRows(numberOfRows, pascalTriangle) {
     if (numberOfRows === 0) return pascalTriangle;
     let newRow = [1];
-    if (pascalTriangle.length === 0) {
-        pascalTriangle.push(newRow);
-        return computeRows(--numberOfRows, pascalTriangle);
-    }
 
-    let previousRow = pascalTriangle[pascalTriangle.length - 1];
+    let previousRow = pascalTriangle[pascalTriangle.length - 1] || [];
     for (let i = 1; i < previousRow.length; i++) {
         newRow.push(previousRow[i] + previousRow[i - 1]);
     }
 
-    newRow.push(1);
+    if (pascalTriangle.length !== 0) newRow.push(1)
     pascalTriangle.push(newRow);
 
     return computeRows(--numberOfRows, pascalTriangle);
 }
 
 export const rows = (numberOfRows) => {
-    return computeRows(numberOfRows, []);
+    return computeRows(numberOfRows, [] );
 };
