b5470f7a7c0c6496bdc0172fcea489ee0d234783
axel-paillaud <contact@axelweb.fr>
2023-10-11 06:46:16 +0200

    refacto pascals-triangle to remove the last condition, quite happy with the result

--- javascript/pascals-triangle/pascals-triangle.js
+++ javascript/pascals-triangle/pascals-triangle.js
@@ -5,16 +5,15 @@
 
 function computeRows(numberOfRows, pascalTriangle) {
     if (numberOfRows === 0) return pascalTriangle;
-    let newRow = [1];
 
-    let previousRow = pascalTriangle[pascalTriangle.length - 1] || [];
-    for (let i = 1; i < previousRow.length; i++) {
-        newRow.push(previousRow[i] + previousRow[i - 1]);
+    const newRow = [];
+    const previousRow = pascalTriangle[pascalTriangle.length - 1] || [];
+
+    for (let i = 0; i <= previousRow.length; i++) {
+        newRow.push((previousRow[i] + previousRow[i - 1]) || 1);
     }
 
-    if (pascalTriangle.length !== 0) newRow.push(1);
     pascalTriangle.push(newRow);
-
     return computeRows(--numberOfRows, pascalTriangle);
 }
 
