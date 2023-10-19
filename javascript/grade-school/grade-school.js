c87ef4a5b6e00105baadce4c102ff76f241b1f87
axel-paillaud <contact@axelweb.fr>
2023-10-19 07:24:01 +0200

    init grade-school, create constructor studentsList

---
+++ javascript/grade-school/grade-school.js
@@ -0,0 +1,22 @@
+//
+// This is only a SKELETON file for the 'Grade School' exercise. It's been provided as a
+// convenience to get you started writing code faster.
+//
+
+export class GradeSchool {
+    constructor () {
+        this.studentsList = {};
+    }
+
+    roster() {
+        return this.studentsList;
+    }
+
+    add() {
+        throw new Error('Remove this statement and implement this function');
+    }
+
+    grade() {
+        throw new Error('Remove this statement and implement this function');
+    }
+}
