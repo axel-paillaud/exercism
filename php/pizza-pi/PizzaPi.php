1c43fd50c8f39f2653d21c56c841ac8f7d4dbf5d
axel-paillaud <contact@axelweb.fr>
2024-06-01 19:10:43 +0200

    pizzaPi wip

--- php/pizza-pi/PizzaPi.php
+++ php/pizza-pi/PizzaPi.php
@@ -1,20 +1,29 @@
 <?php
 
+const SAUCE_PER_PIZZA = 125;
+const DOUGH_PER_PIZZA = 200;
+const PI = 3.14;
+
 class PizzaPi
 {
-    public function calculateDoughRequirement()
+    public function calculateDoughRequirement($pizzas, $persons)
     {
-        throw new \BadFunctionCallException("Implement the function");
+        $more_dough_required_per_person = 20;
+        return $pizzas * (($persons * $more_dough_required_per_person) + DOUGH_PER_PIZZA);
     }
 
-    public function calculateSauceRequirement()
+    public function calculateSauceRequirement($pizzas, $canSize)
     {
-        throw new \BadFunctionCallException("Implement the function");
+        return $pizzas * SAUCE_PER_PIZZA / $canSize;
     }
 
-    public function calculateCheeseCubeCoverage()
+    public function calculateCheeseCubeCoverage(
+        $cheese_dimension, 
+        $cheese_thickness, 
+        $pizza_diameter
+    )
     {
-        throw new \BadFunctionCallException("Implement the function");
+        return floor(($cheese_dimension**3) / ($cheese_thickness * PI * $pizza_diameter));
     }
 
     public function calculateLeftOverSlices()
