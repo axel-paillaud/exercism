6f07665a148a523fa11565b8898e16f05a7bba0c
axel-paillaud <contact@axelweb.fr>
2024-06-06 18:15:57 +0200

    sumUp ok

--- php/lucky-numbers/LuckyNumbers.php
+++ php/lucky-numbers/LuckyNumbers.php
@@ -4,7 +4,18 @@ class LuckyNumbers
 {
     public function sumUp(array $digitsOfNumber1, array $digitsOfNumber2): int
     {
-        throw new \BadFunctionCallException("Implement the function");
+        $letters_1 = '';
+        $letters_2 = '';
+
+        foreach ($digitsOfNumber1 as $digit) {
+            $letters_1 .= (string) $digit;
+        } 
+
+        foreach ($digitsOfNumber2 as $digit) {
+            $letters_2 .= (string) $digit;
+        }
+
+        return (int) $letters_1 + (int) $letters_2;
     }
 
     public function isPalindrome(int $number): bool
